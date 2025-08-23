1d82499787f2e586764953ccd213eea54ff5073a
ChampionNan <bingnan.chen@connect.ust.hk>
2025-08-23 13:38:30 +0000

    Minor

--- query/graph/q2a/rewrite0.sql
+++ query/graph/q2a/rewrite0.sql
@@ -1,8 +1,8 @@
 create or replace view bag134 as select g5.src as v11, g6.src as v14, g5.weight as v15, g6.dst as v17, g6.weight as v18 from bitcoin as g6, bitcoin as g5 where g6.src=g5.dst;
-create or replace view semiJoinView1887810401041340143 as select src as v17, dst as v11, weight as v12 from bitcoin AS g4 where (dst, src) in (select v11, v17 from bag134);
+create or replace view semiJoinView1887810401041340143 as select src as v17, dst as v11, weight as v12 from bitcoin AS g4 where (dst, src) in (select (v11, v17) from bag134);
 create or replace view semiJoinView1737440276379008321 as select src as v2, dst as v17, weight as v21 from bitcoin AS g7 where (dst) in (select v17 from semiJoinView1887810401041340143);
 create or replace view bag135 as select g2.src as v2, g3.src as v5, g2.weight as v6, g3.dst as v8, g3.weight as v9 from bitcoin as g3, bitcoin as g2 where g3.src=g2.dst;
-create or replace view semiJoinView2317624826007240179 as select src as v8, dst as v2, weight as v3 from bitcoin AS g1 where (src, dst) in (select v8, v2 from bag135) and weight<2;
+create or replace view semiJoinView2317624826007240179 as select src as v8, dst as v2, weight as v3 from bitcoin AS g1 where (src, dst) in (select (v8, v2) from bag135) and weight<2;
 create or replace view semiJoinView5375108191846730117 as select v2, v17, v21 from semiJoinView1737440276379008321 where (v2) in (select v2 from semiJoinView2317624826007240179);
 create or replace view semiEnum4950353681819139933 as select v21, v8, v3, v17, v2 from semiJoinView5375108191846730117 join semiJoinView2317624826007240179 using(v2);
 create or replace view semiEnum3931075297898654660 as select v6, v21, v8, v9, v3, v5, v17, v2 from semiEnum4950353681819139933 join bag135 using(v8, v2);
