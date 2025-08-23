8e883c69e9015b0395d868a92e608aea3efa2241
CHEN Bingnan <bchenba@CSs-Mac-Studio.local>
2025-08-23 16:53:56 +0800

    Add rewruteYa.sql

---
+++ query/graph/q3/rewriteYa0.sql
@@ -0,0 +1,10 @@
+create or replace TEMP view g3 as select Graph.src as v4, Graph.dst as v9, v10, v14 from Graph, (SELECT src, COUNT(*) AS v10 FROM Graph GROUP BY src) AS c2, (SELECT dst, COUNT(*) AS v14 FROM Graph GROUP BY dst) AS c4 where Graph.dst = c2.src and Graph.dst = c4.dst;
+create or replace TEMP view g2 as select Graph.src as v2, Graph.dst as v4, v12 from Graph, (SELECT src, COUNT(*) AS v12 FROM Graph GROUP BY src) AS c3 where Graph.src = c3.src;
+create or replace TEMP view semiUp5506524812506105508 as select v2, v4, v12 from g2 where (v4) in (select (v4) from g3);
+create or replace TEMP view g1 as select Graph.src as v7, Graph.dst as v2, v8 from Graph, (SELECT src, COUNT(*) AS v8 FROM Graph GROUP BY src) AS c1 where Graph.src = c1.src and v8<5;
+create or replace TEMP view semiUp7675917132461008855 as select v2, v4, v12 from semiUp5506524812506105508 where (v2) in (select (v2) from g1);
+create or replace TEMP view semiDown2355769092476194387 as select v7, v2, v8 from g1 where (v2) in (select (v2) from semiUp7675917132461008855);
+create or replace TEMP view semiDown1475262629368342022 as select v4, v9, v10, v14 from g3 where (v4) in (select (v4) from semiUp7675917132461008855);
+create or replace TEMP view joinView3687824337161371304 as select v2, v4, v12, v7, v8 from semiUp7675917132461008855 join semiDown2355769092476194387 using(v2);
+create or replace TEMP view joinView3059479926151137968 as select v2, v4, v12, v7, v8, v9, v10, v14 from joinView3687824337161371304 join semiDown1475262629368342022 using(v4);
+select v7, v2, v4, v9, v8, v10, v12, v14 from joinView3059479926151137968;
